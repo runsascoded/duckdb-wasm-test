422ae06467d547afb97486e2978a947af0eddd4b
Ryan Williams <ryan@runsascoded.com>
2024-01-03 22:12:51 -0500

    db <select>, suspense load

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,47 +1,71 @@
-import { HTMLProps, useCallback, useEffect, useRef, useState } from "react";
+import React, { ChangeEventHandler, Dispatch, HTMLProps, useCallback, useEffect, useRef, useState } from "react";
 import { initDuckDb, runQuery } from "next-utils/parquet"
 import { AsyncDuckDB } from "@duckdb/duckdb-wasm";
 import useSessionStorageState from 'use-session-storage-state'
 import css from "./index.module.scss"
 
+const dbUrls = [
+    's3://duckdb-repl/1.duckdb',
+    's3://duckdb-repl/1-idx.duckdb',
+    's3://duckdb-repl/2.duckdb',
+    's3://duckdb-repl/2-idx.duckdb',
+    's3://duckdb-repl/3.duckdb',
+    's3://duckdb-repl/3-idx.duckdb',
+]
 
 const QUERY_KEY = 'duckdb-query'
 const DEFAULT_QUERY = `select count(*) from crashes`
 const DB_URL_KEY = 'duckdb-db-url'
-const DB_URL_VAL_KEY = 'duckdb-db-url-val'
-const DEFAULT_DB_URL = "s3://nj-crashes/njdot/2021.duckdb"
+const DEFAULT_DB_URL = dbUrls[0]
 const RESULT_KEY = 'duckdb-result'
 
 export function Textarea(
-    { title, storageKey, defaultValue = "", ...props }: {
+    { title, storageKey, defaultValue = "", value, setValue, onChange, ...props }: {
         title: string
         storageKey: string
         defaultValue?: string
-    } & Partial<HTMLProps<HTMLTextAreaElement>>
+        value?: string
+        setValue?: Dispatch<string>
+        onChange?: ChangeEventHandler<HTMLTextAreaElement> | undefined
+    } & Partial<Omit<HTMLProps<HTMLTextAreaElement>, 'onChange'>>
 ) {
-    const [ value, setValue ] = useSessionStorageState<string>(storageKey, { defaultValue })
+    if (value === undefined || setValue === undefined) {
+        if (value === undefined && setValue === undefined) {
+            [ value, setValue ] = useSessionStorageState<string>(storageKey, { defaultValue })
+        } else{
+            throw new Error("either both value and setValue must be defined or neither")
+        }
+    }
+
     return <>
-        <h2>{title}:</h2>
+        <h2>{title}</h2>
         <textarea
             className={css.query}
             value={value}
             onChange={e => {
+                if (onChange) {
+                    onChange(e)
+                }
                 const value = (e.target as HTMLTextAreaElement).value
-                setValue(value)
+                setValue && setValue(value)
             }}
             {...props}
         />
     </>
 }
 
-export default function Page() {
+export function Repl() {
     const [ dbUrl, setDbUrl ] = useSessionStorageState<string>(DB_URL_KEY, { defaultValue: DEFAULT_DB_URL })
     const [ db, setDb ] = useState<AsyncDuckDB | null>(null)
-    const ref = useRef<HTMLTextAreaElement>(null);
+    const [ query, setQuery ] = useSessionStorageState<string>(QUERY_KEY, { defaultValue: DEFAULT_QUERY })
     const [ result, setResult ] = useSessionStorageState<any>(RESULT_KEY, { defaultValue: null })
     useEffect(
         () => {
-            initDuckDb({ path: dbUrl, }).then(db => setDb(db))
+            console.log("initializing db:", dbUrl)
+            initDuckDb({ path: dbUrl, }).then(db => {
+                console.log("setting new db")
+                setDb(db)
+            })
         },
         [ dbUrl ]
     )
@@ -51,43 +75,51 @@ export default function Page() {
                 console.error("no db")
                 return
             }
-            const query = ref.current?.value
             if (!query) {
                 return
             }
+            setResult(null)
             console.log("running query:", query)
             runQuery(db, query).then(result => {
                 console.log("result:", result)
                 setResult(result)
             })
         },
+        [ db, query ]
+    )
+    useEffect(
+        () => {
+            console.log("db effect:", db)
+            if (!db) return
+            run()
+        },
         [ db ]
     )
     return <div className={css.main}>
         <div className={css.row}>
-            <Textarea
-                title={"Database"}
-                storageKey={DB_URL_VAL_KEY}
-                defaultValue={DEFAULT_DB_URL}
+            <select
                 className={css.dbUrl}
-                onKeyDown={e => {
-                    if (e.code === 'Enter') {
-                        e.preventDefault();
-                        const newDb = (e.target as HTMLTextAreaElement).value
-                        console.log("new db:", newDb)
-                        setDbUrl(newDb)
-                    }
+                value={dbUrl}
+                onChange={e => {
+                    const newDb = (e.target as HTMLSelectElement).value
+                    console.log("new db:", newDb)
+                    setDbUrl(newDb)
+                    setDb(null)
+                    setResult(null)
                 }}
-            />
+            >{
+                dbUrls.map(dbUrl => <option key={dbUrl} value={dbUrl}>{dbUrl}</option>)
+            }</select>
         </div>
         <div className={css.row}>
             <Textarea
                 title={"Query"}
                 className={css.query}
+                value={query}
+                setValue={setQuery}
                 storageKey={QUERY_KEY}
                 defaultValue={DEFAULT_QUERY}
                 onKeyDown={e => {
-                    // console.log("key:", e.code, e)
                     if (e.code === 'Enter' && e.shiftKey) {
                         e.preventDefault();
                         run()
@@ -105,11 +137,23 @@ export default function Page() {
             </button>
         </div>
         <div className={css.row}>
-            <h2>Result:</h2>
+            <h2>Result</h2>
             <pre>{
-                result &&
+                result && db &&
                 JSON.stringify(result, null, 2)
             }</pre>
         </div>
     </div>
+}
+
+export default function Page() {
+    const [ body, setBody ] = useState<JSX.Element | null>(null)
+    useEffect(
+        () => {
+            console.log("setting body")
+            setBody(<Repl />)
+        },
+        []
+    )
+    return body
 }
