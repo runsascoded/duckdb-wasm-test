3b7614f3c1056e1eeda81bc476de5ab708c2895a
ryan-williams <nobigdealstyle@gmail.com>
2024-01-08 02:14:32 +0000

    Deploying to gh-pages from @ runsascoded/duckdb-wasm-test@df9adf16a2a8a9e1741385360561831fa764058f 🚀

---
+++ _next/static/tnoeRcm9-goN6cjII8A_8/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/592-884d5b785d957303.js","static/css/0f313751ffc0e43a.css","static/chunks/pages/index-76a76ffed6f6964a.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
